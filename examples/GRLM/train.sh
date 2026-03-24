7475bfaafc2e55c02a912f0be75fe79195a15318
coco <714366968@qq.com>
2026-03-24 05:40:41 +0000

    k

--- examples/GRLM/train.sh
+++ examples/GRLM/train.sh
@@ -2,3 +2,5 @@
 # DO NOT use GPTQ/AWQ model in FSDP+QLoRA
 
 DISABLE_VERSION_CHECK=1 nohup accelerate launch --config_file ./examples/GRLM/fsdp_config.yaml src/train.py ./examples/GRLM/shopping_sft.yaml > ./logs/qwen35-9b_fsdp_qlora.out 2>&1 &
+
+DISABLE_VERSION_CHECK=1 PYTHONPATH=src nohup python -u -m llamafactory.cli export examples/GRLM/shopping_merge_lora.yaml > ./logs/merge_lora.out 2>&1 &
